17ab6529ced9c8f75a219b4628f31d93bab1396e
Radoslav Bekyarov <rbekyarov@students.softuni.bg>
2023-09-03 16:55:44 +0300

    Added Brand.class and Model.class

--- src/main/java/rbekyarov/car_dealership/models/entity/Brand.java
+++ src/main/java/rbekyarov/car_dealership/models/entity/Brand.java
@@ -1,4 +1,27 @@
 package rbekyarov.car_dealership.models.entity;
 
-public class Brand {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "brands")
+public class Brand extends BaseEntity {
+    private String name;
+
+    public Brand() {
+    }
+
+    public Brand(String name) {
+        this.name = name;
+    }
+
+    @Column(unique = true, nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/rbekyarov/car_dealership/models/entity/Model.java
+++ src/main/java/rbekyarov/car_dealership/models/entity/Model.java
@@ -1,4 +1,31 @@
 package rbekyarov.car_dealership.models.entity;
 
-public class Model {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "models")
+public class Model extends BaseEntity{
+    private String name;
+    private Brand brand;
+
+    public Model() {
+    }
+
+    @Column(nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @ManyToOne
+    @JoinColumn(name = "brand_id", referencedColumnName = "id")
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
 }
