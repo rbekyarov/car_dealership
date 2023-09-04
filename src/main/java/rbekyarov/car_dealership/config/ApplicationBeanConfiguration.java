1e8af9eb5ebe4a0b2961f849d37cef20512ee2cb
Radoslav Bekyarov <rbekyarov82@gmail.com>
2023-09-04 16:45:46 +0300

    Implement CRUD for USER entity

--- src/main/java/rbekyarov/car_dealership/config/ApplicationBeanConfiguration.java
+++ src/main/java/rbekyarov/car_dealership/config/ApplicationBeanConfiguration.java
@@ -3,6 +3,8 @@ package rbekyarov.car_dealership.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration
 public class ApplicationBeanConfiguration {
@@ -11,5 +13,9 @@ public class ApplicationBeanConfiguration {
         ModelMapper modelMapper = new ModelMapper();
 
         return modelMapper;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Pbkdf2PasswordEncoder();
     }
 }
