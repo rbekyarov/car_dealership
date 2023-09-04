e297bb9866c66525e4b1c64f4986f4c79b2a8af5
Radoslav Bekyarov <rbekyarov82@gmail.com>
2023-09-04 15:31:56 +0300

    Added Services Interfaces and Services Impls

---
+++ src/main/java/rbekyarov/car_dealership/services/impl/PictureServiceImpl.java
@@ -0,0 +1,7 @@
+package rbekyarov.car_dealership.services.impl;
+
+import org.springframework.stereotype.Service;
+import rbekyarov.car_dealership.services.PictureService;
+@Service
+public class PictureServiceImpl implements PictureService {
+}
---
+++ src/main/java/rbekyarov/car_dealership/services/impl/VendorServiceImpl.java
@@ -0,0 +1,7 @@
+package rbekyarov.car_dealership.services.impl;
+
+import org.springframework.stereotype.Service;
+import rbekyarov.car_dealership.services.VendorService;
+@Service
+public class VendorServiceImpl implements VendorService {
+}
