a2184f86dea8c02101c024da11f7965f03b554bf
Radoslav Bekyarov <rbekyarov@students.softuni.bg>
2023-09-07 18:09:37 +0300

    add test constructor

--- src/main/java/rbekyarov/car_dealership/models/dto/ClientDTO.java
+++ src/main/java/rbekyarov/car_dealership/models/dto/ClientDTO.java
@@ -16,6 +16,10 @@ public class ClientDTO {
     public ClientDTO() {
     }
 
+    public ClientDTO(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
