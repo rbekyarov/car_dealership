dae2f0a0be26748f707274ee8b0b7eebbaf88ea2
Radoslav Bekyarov <rbekyarov@students.softuni.bg>
2023-09-08 18:06:46 +0300

    Add BrandController - crud

---
+++ src/main/java/rbekyarov/car_dealership/controllers/BrandController.java
@@ -0,0 +1,44 @@
+package rbekyarov.car_dealership.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import rbekyarov.car_dealership.models.dto.BrandDTO;
+import rbekyarov.car_dealership.models.entity.Brand;
+import rbekyarov.car_dealership.services.BrandService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/brands")
+public class BrandController {
+
+    @Autowired
+    private BrandService brandService;
+
+    @GetMapping
+    public List<Brand> getAllClients() {
+        return brandService.findAllBrands();
+    }
+
+    @GetMapping("/{id}")
+    public Brand getBrandById(@PathVariable Long id) {
+        return brandService.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    void createBrand(@RequestBody BrandDTO brandDTO, HttpSession session) {
+        brandService.addBrand(brandDTO,session);
+    }
+
+    @PutMapping("/{id}")
+    void updateClient(@PathVariable Long id, @RequestBody BrandDTO brandDTO, HttpSession session) {
+         brandService.editBrand(brandDTO.getName(), id,session);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteClient(@PathVariable Long id) {
+        brandService.removeBrandById(id);
+    }
+}
