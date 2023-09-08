0b695f5b1eb78b5da574242de48f63256462cc61
Radoslav Bekyarov <rbekyarov@students.softuni.bg>
2023-09-08 22:44:31 +0300

    fix controller

--- src/main/java/rbekyarov/car_dealership/controllers/BrandController.java
+++ src/main/java/rbekyarov/car_dealership/controllers/BrandController.java
@@ -1,14 +1,22 @@
 package rbekyarov.car_dealership.controllers;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import rbekyarov.car_dealership.exception.ResourceNotFoundException;
 import rbekyarov.car_dealership.models.dto.BrandDTO;
 import rbekyarov.car_dealership.models.entity.Brand;
 import rbekyarov.car_dealership.services.BrandService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/brands")
 public class BrandController {
@@ -17,28 +25,48 @@ public class BrandController {
     private BrandService brandService;
 
     @GetMapping
-    public List<Brand> getAllClients() {
-        return brandService.findAllBrands();
+    public ResponseEntity<?>getAllBrands() {
+        List<Brand> allBrands = brandService.findAllBrands();
+        return new ResponseEntity<>(allBrands, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Brand getBrandById(@PathVariable Long id) {
-        return brandService.findById(id).orElseThrow();
+    public ResponseEntity<?> getBrandById(@PathVariable Long id) {
+        Brand brand = brandService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Brand not exist with id: " + id));
+        return new ResponseEntity<>(brand, HttpStatus.OK);
     }
 
     @PostMapping
-    void createBrand(@RequestBody BrandDTO brandDTO, HttpSession session) {
-        brandService.addBrand(brandDTO,session);
+    public ResponseEntity<?> createBrand(@RequestBody @Valid BrandDTO brandDTO, HttpSession session, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
+
+            return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
+        } else {
+            brandService.addBrand(brandDTO,session);
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
     }
 
     @PutMapping("/{id}")
-    void updateBrand(@PathVariable Long id, @RequestBody BrandDTO brandDTO, HttpSession session) {
-         brandService.editBrand(brandDTO.getName(), id,session);
+    public ResponseEntity<?>updateBrand(@PathVariable Long id, @RequestBody @Valid BrandDTO brandDTO, HttpSession session, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
 
+            return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
+        } else {
+            brandService.editBrand(brandDTO.getName(), id, session);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteBrand(@PathVariable Long id) {
+    public ResponseEntity<Map<String, Boolean>> deleteBrand(@PathVariable Long id) {
         brandService.removeBrandById(id);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/rbekyarov/car_dealership/controllers/ClientController.java
+++ src/main/java/rbekyarov/car_dealership/controllers/ClientController.java
@@ -1,8 +1,13 @@
 package rbekyarov.car_dealership.controllers;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import rbekyarov.car_dealership.exception.ResourceNotFoundException;
 import rbekyarov.car_dealership.models.dto.BrandDTO;
 import rbekyarov.car_dealership.models.dto.ClientDTO;
 import rbekyarov.car_dealership.models.entity.Brand;
@@ -12,8 +17,11 @@ import rbekyarov.car_dealership.services.BrandService;
 import rbekyarov.car_dealership.services.ClientService;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
@@ -22,37 +30,56 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping
-    public List<Client> getAllClients() {
-        return clientService.findAllClients();
+    public ResponseEntity<?> getAllClients() {
+        List<Client> allClients = clientService.findAllClients();
+        return new ResponseEntity<>(allClients, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Client getClientById(@PathVariable Long id) {
-        return clientService.findById(id).orElseThrow();
+    public ResponseEntity<?> getClientById(@PathVariable Long id) {
+        Client client = clientService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Client not exist with id: " + id));
+        return new ResponseEntity<>(client, HttpStatus.OK);
     }
 
     @PostMapping
-    void createClient(@RequestBody ClientDTO clientDTO, HttpSession session) {
-        clientService.addClient(clientDTO, session);
+    public ResponseEntity<?> createClient(@RequestBody @Valid ClientDTO clientDTO, HttpSession session, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
+
+            return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
+        } else {
+            clientService.addClient(clientDTO,session);
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
     }
 
     @PutMapping("/{id}")
-    void updateClient(@PathVariable Long id, @RequestBody ClientDTO clientDTO, HttpSession session) {
+    public ResponseEntity<?>updateClient(@PathVariable Long id, @RequestBody @Valid ClientDTO clientDTO, HttpSession session, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
 
-        String name = clientDTO.getName();
-        String vatOrId = clientDTO.getVatOrId();
-        String email = clientDTO.getEmail();
-        String phone = clientDTO.getPhone();
-        String city = clientDTO.getCity();
-        String address = clientDTO.getAddress();
-        ClientType clientType = clientDTO.getClientType();
+            return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
+        } else {
+            String name = clientDTO.getName();
+            String vatOrId = clientDTO.getVatOrId();
+            String email = clientDTO.getEmail();
+            String phone = clientDTO.getPhone();
+            String city = clientDTO.getCity();
+            String address = clientDTO.getAddress();
+            ClientType clientType = clientDTO.getClientType();
 
-        clientService.editClient(name, email, vatOrId, phone, city, address, clientType, id, session);
-
+            clientService.editClient(name, email, vatOrId, phone, city, address, clientType, id, session);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteClient(@PathVariable Long id) {
+    public ResponseEntity<Map<String, Boolean>> deleteClient(@PathVariable Long id) {
         clientService.removeClientById(id);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
     }
 }
